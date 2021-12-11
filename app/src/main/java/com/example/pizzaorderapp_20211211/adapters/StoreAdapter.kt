eff6d9ce5bf8e41e415b04485fea277a4c925b04
김창배 <cbkim93@naver.com>
2021-12-11 13:37:30 +0900

    getView 함수 완성 - 기본 모양 inflate -> 바로 내보내기

--- app/src/main/java/com/example/pizzaorderapp_20211211/adapters/StoreAdapter.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211211/adapters/StoreAdapter.kt
@@ -1,8 +1,12 @@
 package com.example.pizzaorderapp_20211211.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Adapter
 import android.widget.ArrayAdapter
+import com.example.pizzaorderapp_20211211.R
 import com.example.pizzaorderapp_20211211.datas.StoreData
 
 class StoreAdapter(
@@ -11,4 +15,17 @@ class StoreAdapter(
     val mList: ArrayList<StoreData>
 ): ArrayAdapter<StoreData>(mContext, resId, mList) {
 
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 }
