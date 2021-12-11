add4636c31104315c7ffd6f2aab36c804ae654e8
김창배 <cbkim93@naver.com>
2021-12-11 14:59:39 +0900

    전화 걸기 처리중 - 권한 설정

--- app/src/main/java/com/example/pizzaorderapp_20211211/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211211/ViewStoreDetailActivity.kt
@@ -1,9 +1,18 @@
 package com.example.pizzaorderapp_20211211
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.example.pizzaorderapp_20211211.datas.StoreData
+import com.gun0912.tedpermission.PermissionListener
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+    lateinit var mStoreData : StoreData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
@@ -13,10 +22,29 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+        btnCall.setOnClickListener {
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
 
+                    val callUri = Uri.parse("tel:${mStoreData.phone}")
+                    val callIntent = Intent(Intent.ACTION_CALL, callUri)
+                    startActivity(callIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(this@ViewStoreDetailActivity, "통화 권한 거부", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
+        }
     }
 
     fun setValues() {
+        mStoreData = intent.getSerializableExtra("store") as StoreData
 
+        Glide.with(this).load(mStoreData.logoURL).centerCrop().into(imgLogo)
+        txtStoreName.text = mStoreData.name
+        txtPhoneNum.text = mStoreData.phone
     }
 }
