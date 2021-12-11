c57d95c0a2291d3de0607ccd07ea367bbb4cf756
김창배 <cbkim93@naver.com>
2021-12-11 12:22:06 +0900

    뷰페이저 어댑터 기초 작업중

---
+++ app/src/main/java/com/example/pizzaorderapp_20211211/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.pizzaorderapp_20211211.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+    }
+}
