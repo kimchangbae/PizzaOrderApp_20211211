c133b1c3ad159f671586f467d5539b5ff3278b15
김창배 <cbkim93@naver.com>
2021-12-11 12:25:14 +0900

    뷰페이저 어댑터 작업 마무리 - 페이지 별 프래그먼트 지정

--- app/src/main/java/com/example/pizzaorderapp_20211211/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211211/adapters/MainViewPagerAdapter.kt
@@ -3,14 +3,17 @@ package com.example.pizzaorderapp_20211211.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.pizzaorderapp_20211211.fragments.MyProfileFragment
+import com.example.pizzaorderapp_20211211.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
-    override fun getCount(): Int {
-
-    }
+    override fun getCount() = 2
 
     override fun getItem(position: Int): Fragment {
-
+        return when(position) {
+            0       -> PizzaStoreListFragment()
+            else    -> MyProfileFragment()
+        }
     }
 }
